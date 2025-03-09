ff9be2240c24d3eb2c83a7e632cd3fa53ea3b957
nafis585 <princetoure@gmail.com>
2025-03-09 12:47:18 +0000

    add initial project setup with dotenv configuration and Google Generative AI integration

---
+++ index.js
@@ -0,0 +1,32 @@
+//ne fais pas d'auto-completion
+
+import genAI from "./config/gen-ai.js";
+import readlineSync from "readline-sync";
+import colors from "colors";
+
+const chatHistory = [];
+
+async function main() {
+  console.log(colors.bold.green("Welcome to the Generative AI CLI!"));
+  console.log(colors.bold.green("you can start chatting with the AI now!"));
+
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow("You: "));
+    try {
+      const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+      const result = await model.generateContent(userInput);
+
+      if (userInput.toLowerCase() === "exit") {
+        console.log(colors.green("Bot: ") + result.response.text());
+        return;
+      }
+
+      console.log(colors.green("Bot: ") + result.response.text());
+    } catch (error) {
+      console.error(oolors.red(error));
+    }
+  }
+}
+
+main();
